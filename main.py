a8e7341b8486cda7cbf882ffb35983551482b0f5
fiscaldefender <suporte3@fiscaldefender.com.br>
2021-09-25 10:44:03 -0300

    Metodo para chamar a tela de Aluguel

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from views.telaprincipal import Ui_MainWindow
 from PyQt5 import QtWidgets
 from controller.vendas import Vendas
 from controller.cadastrodeveiculos import CadastroVeiculos
+from controller.AlugarVeiculoController import AlugarVeiculo
 import sys
 import logging
 import os
@@ -16,7 +17,9 @@ class AlugarApp(QtWidgets.QMainWindow):# Classe que inicializa o sistema
         self.ui.setupUi(self)
         self.ui.venderCarro.clicked.connect(self.venderveiculo)
         self.ui.cadastrarCarro.clicked.connect(self.cadastrarveiculo)
-        
+        self.ui.alugarCarro.clicked.connect(self.alugarveiculo)
+        self.ui.actionCarros_usados.triggered.connect(self.venderveiculo)
+        self.ui.actionCarros_novos.triggered.connect(self.venderveiculo)
         
     def venderveiculo(self):
         self.window = Vendas()
@@ -27,6 +30,10 @@ class AlugarApp(QtWidgets.QMainWindow):# Classe que inicializa o sistema
         self.window = CadastroVeiculos()
         self.window.show()
     
+    def alugarveiculo(self):
+        self.window = AlugarVeiculo()
+        self.window.show()
+
 
 
 if __name__ == "__main__":
